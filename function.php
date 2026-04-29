c0dc2938142f962a6d2d347e60fb8f4b2792aab9
wibisanabama <wibisanabama@gmail.com>
2026-04-29 08:07:59 +0700

    membuat fitur tambah data user

--- function.php
+++ function.php
@@ -68,3 +68,21 @@ function hapus_tamu($id) {
 
     return mysqli_affected_rows($koneksi);
 }
+
+function tambah_user($data) {
+    global $koneksi;
+
+    $kode           = htmlspecialchars($data['id_user']);
+    $username       = htmlspecialchars($data['username']);
+    $password       = htmlspecialchars($data['password']);
+    $user_role      = htmlspecialchars($data['user_role']);
+
+    // enkripsi password dengan password_hash
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $query = "INSERT INTO users VALUES ('$kode', '$username', '$password_hash', '$user_role')";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
--- users.php
+++ users.php
@@ -12,7 +12,7 @@ include_once('templates/header.php');
     <?php
     // jika ada tombol simpan
     if (isset($_POST['simpan'])) {
-        if (tambah_tamu($_POST) > 0) {
+        if (tambah_user($_POST) > 0) {
     ?>
             <div class="alert alert-success" role="alert">
                 Data berhasil disimpan!
@@ -77,12 +77,12 @@ include_once('templates/header.php');
 <!-- /.container-fluid -->
  <?php
     // mengambil data barang dari tabel dengan kode terbesar
-    $query = mysqli_query($koneksi, "SELECT max(id_tamu) as kodeTerbesar FROM buku_tamu");
+    $query = mysqli_query($koneksi, "SELECT max(id_user) as kodeTerbesar FROM users");
     $data = mysqli_fetch_array($query);
-    $kodeTamu = $data['kodeTerbesar'];
+    $kodeuser = $data['kodeTerbesar'];
 
     // mengambil angka dari kode barang terbesar, menggunakan fungsi substr dan diubah ke integer dengan (int)
-    $urutan = (int) substr($kodeTamu, 2, 3);
+    $urutan = (int) substr($kodeuser, 3, 2);
 
     // nomor yang diambil akan ditambah 1 untuk menentukan nomor urut berikutnya
     $urutan++;
@@ -91,50 +91,41 @@ include_once('templates/header.php');
     // string sprintf("%03s", $urutan); berfungsi untuk membuat string menjadi 3 karakter
 
     // angka yang diambil tadi digabungkan dengan kode huruf yang kita inginkan, misalnya zt
-    $huruf = "zt";
-    $kodeTamu = $huruf . sprintf("%03s", $urutan);
+    $huruf = "usr";
+    $kodeuser = $huruf . sprintf("%02s", $urutan);
     ?>
 <!-- Modal Tambah -->
 <div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="tambahModalLabel">Tambah Data Tamu</h5>
+                <h5 class="modal-title" id="tambahModalLabel">Tambah Data user</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
               <form method="post" action="">
-                    <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $kodeTamu ?>">
+                    <input type="hidden" name="id_user" id="id_user" value="<?= $kodeuser ?>">
                     <div class="form-group row">
-                        <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
+                        <label for="username" class="col-sm-3 col-form-label">Username</label>
                         <div class="col-sm-8">
-                            <input type="text" class="form-control" id="nama_tamu" name="nama_tamu">
+                            <input type="text" class="form-control" id="username" name="username">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
+                        <label for="password" class="col-sm-3 col-form-label">Password</label>
                         <div class="col-sm-8">
-                            <textarea class="form-control" id="alamat" name="alamat"></textarea>
+                            <input type="password" class="form-control" id="password" name="password">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="no_hp" class="col-sm-3 col-form-label">No. Telepon</label>
+                        <label for="user_role" class="col-sm-3 col-form-label">User Role</label>
                         <div class="col-sm-8">
-                            <input type="text" class="form-control" id="no_hp" name="no_hp">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="bertemu" class="col-sm-3 col-form-label">Bertemu dg. </label>
-                        <div class="col-sm-8">
-                            <input type="text" class="form-control" id="bertemu" name="bertemu">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
-                        <div class="col-sm-8">
-                            <input type="text" class="form-control" id="kepentingan" name="kepentingan">
+                            <select class="form-control" id="user_role" name="user_role">
+                                <option value="admin">Administrator</option>
+                                <option value="operator">Operator</option>
+                            </select>
                         </div>
                     </div>
             </div>
